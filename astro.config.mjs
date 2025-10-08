6c947aff9ff064931cae5574e2651f7e69c46e37
EDGAR CLIMACO MARTINEZ <edgar.climaco@academia-ciberseguridad.com>
2025-10-08 00:55:04 -0600

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'http://localhost:4321',
-  server: { host: true, port: 4321, open: true },
-  output: 'static',
-  devToolbar: { enabled: false } // adiós error del toolbar
-});
