eb7e287da0f2f64cfd552d9d2a69cb6dece5aec7
edgar <edgar@kali>
2025-10-26 20:56:43 -0600

    Fix canonical URLs and meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// Añadirás site y la integración de sitemap cuando apruebes "go live".
 export default defineConfig({
-  // site: 'https://tu-dominio.tld', // definir al publicar
-  integrations: [
-    // import sitemap desde '@astrojs/sitemap' cuando definas `site`
-    // sitemap()
-  ],
+  site: 'https://www.rcpn.me',
+  base: '/',
 });
